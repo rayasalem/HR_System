8757ba2d2f00c677b6d43a257e64d064faa369e8
Rayaalislam Salem <120057726+rayasalem@users.noreply.github.com>
2024-08-31 00:08:04 +0300

    Add files via upload
    addd

---
+++ WebApplication4/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+    public class Employee
+    {
+        [Key] 
+        public int Id { get; set; }
+
+        [Required] 
+        [MaxLength(100)] 
+        public string? Name { get; set; }
+
+        [Required] 
+        [Url] 
+        public string ?Image { get; set; }
+
+        [Required]
+        [MaxLength(100)] 
+        public string? Specialty { get; set; }
+
+        [EmailAddress] 
+        public string ?Email { get; set; }
+
+        [Phone]
+        public string? Phone { get; set; }
+
+        [MaxLength(200)] 
+        public string? Address { get; set; }
+
+        public DateTime? HireDate { get; set; } 
+    }
