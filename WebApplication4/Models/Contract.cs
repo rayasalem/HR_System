3a5cbb7ebf6407a9464f2b2593e86a97bdc359c4
Shaymaa AlKhader <87859824+Shaima-AlKhader@users.noreply.github.com>
2024-08-31 15:09:04 +0300

    Add files via upload

---
+++ WebApplication4/Models/Contract.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Contract
+    {
+        [Key]
+        [Display(Name = "Code")]
+        [Column(TypeName = "int")]
+        public int ID { get; set; }
+
+        [Display(Name = "Contract Type")]
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string? ContractType { get; set; }
+
+        [Display(Name = "Position")]
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string? Position { get; set; }
+
+
+        [Display(Name = "StartDate")]
+        [Column(TypeName = "date")]
+        public DateOnly? StartDate { get; set; }
+
+
+        [Display(Name = "End Date")]
+        [Column(TypeName = "date")]
+        public DateOnly? EndDate { get; set; }
+    }
+}
---
+++ WebApplication4/Models/Department.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication2.Models
+{
+    public class Department
+    {
+        [Key]
+        [Display(Name ="Code")]
+        [Column(TypeName ="int")]
+        public int ID { get; set; }
+
+        [Display(Name = "Department Name")]
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string? DepartmentName { get; set; }
+
+        [Display(Name = "Number Of Employees")]
+        [Column(TypeName = "int")]
+        public static int NumberOfEmployees{ get; set; }
+
+        [Display(Name = "Head of Department")]
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string? DepartmentHead { get; set; }
+
+        [Display(Name = "Location")]
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string? Location { get; set; }
+
+        [Display(Name = "Budget")]
+        [Column(TypeName = "int")]
+        public int? Budget { get; set; }
+
+       
+    }
+}
