f2841933347770c1cb8e0f1d70624e9f28a10a2e
Shaymaa AlKhader <87859824+Shaima-AlKhader@users.noreply.github.com>
2024-09-03 12:47:26 +0300

    Update Project.cs

--- WebApplication4/Models/Project.cs
+++ WebApplication4/Models/Project.cs
@@ -1,7 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
-namespace WebApplication2.Models
+namespace WebApplication4.Models
 {
     public class Project
     {
