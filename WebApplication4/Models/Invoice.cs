c434f5c1b7b5930603e99e6796d66babde8b57f8
Shaymaa AlKhader <87859824+Shaima-AlKhader@users.noreply.github.com>
2024-09-03 12:45:57 +0300

    Update Invoice.cs

--- WebApplication4/Models/Invoice.cs
+++ WebApplication4/Models/Invoice.cs
@@ -1,43 +1,46 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
-namespace WebApplication2.Models;
-public class Invoice
+namespace WebApplication4.Models
 {
-    [Key]
-    [Display(Name = "Code")]
-    [Column(TypeName = "int")]
-    public int ID { get; set; }
 
-    [Display(Name = "Invoice Name")]
-    [Column(TypeName = "nvarchar(MAX)")]
-    public string? InvoiceName { get; set; }
+    public class Invoice
+    {
+        [Key]
+        [Display(Name = "Code")]
+        [Column(TypeName = "int")]
+        public int ID { get; set; }
 
-
-    [Display(Name = "Amount")]
-    [Column(TypeName = "decimal (18, 0)")]
-    public decimal? Amount { get; set; }
-
-    [Display(Name = "Invoice Date")]
-    [Column(TypeName = "date")]
-    public DateOnly? InvoiceDate { get; set; }
+        [Display(Name = "Invoice Name")]
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string? InvoiceName { get; set; }
 
 
-    [Display(Name = "Invoice Due")]
-    [Column(TypeName = "date")]
-    public DateOnly? InvoiceDue { get; set; }
+        [Display(Name = "Amount")]
+        [Column(TypeName = "decimal (18, 0)")]
+        public decimal? Amount { get; set; }
 
-    [Display(Name = "Invoice Description")]
-    [Column(TypeName = "nvarchar(MAX)")]
-    public string? InvoiceDescription { get; set; }
-
-    [Required]
-    [Display(Name = "Status")]
-    [Column(TypeName = "nvarchar(MAX)")]
-    public string? Status { get; set; }
+        [Display(Name = "Invoice Date")]
+        [Column(TypeName = "date")]
+        public DateOnly? InvoiceDate { get; set; }
 
 
-    [ForeignKey("EmpRef")]
-    public int EmpRef { get; set; }
-    public Employee? emps { get; set; }
+        [Display(Name = "Invoice Due")]
+        [Column(TypeName = "date")]
+        public DateOnly? InvoiceDue { get; set; }
+
+        [Display(Name = "Invoice Description")]
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string? InvoiceDescription { get; set; }
+
+        [Required]
+        [Display(Name = "Status")]
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string? Status { get; set; }
+
+
+        [ForeignKey("EmpRef")]
+        public int EmpRef { get; set; }
+        public Employee? emps { get; set; }
+    }
 }
